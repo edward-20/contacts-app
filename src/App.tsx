873c35b7a84324d86b24b25ef8a06a0e1a5c6551
Edward Lim <ed.lim199@gmail.com>
2024-11-21 11:50:06 +1100

    SPA app that fetches static resources on mount

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className='flex flex-row justify-center items-center w-screen min-h-screen'>
+      <h1>Contacts</h1>
+    </div>
+  )
+}
+
+export default App
